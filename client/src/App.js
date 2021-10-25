ac8732bbc40dede35b2846849ea0a78e3e5a3613
Suyash S <suyash988@gmail.com>
2021-10-25 16:56:03 -0400

    client setup cra

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <div className="App">Hello World</div>
+    </>
+  );
+}
+
+export default App;
