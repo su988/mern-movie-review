f28d10c36750706ff072020aae4e55f7e5d175ab
Suyash S <suyash988@gmail.com>
2021-10-26 21:42:46 -0400

    movie detail page

--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -1,9 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import MovieDataService from '../services/movies';
+import { Link } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 
-export const Movie = () => {
+export const Movie = (props) => {
+  const [movie, setMovie] = useState({
+    id: null,
+    title: '',
+    rated: '',
+    reviews: [],
+  });
+
+  const getMovie = (id) => {
+    MovieDataService.get(id)
+      .then((response) => {
+        setMovie(response.data);
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  useEffect(() => {
+    getMovie(props.match.params.id);
+  }, [props.match.params.id]);
+
   return (
     <>
-      <div>Movie</div>
+      <div>
+        <Container>
+          <Row>
+            <Col>
+              <Image
+                src={
+                  movie.poster
+                    ? movie.poster + '/100px250'
+                    : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTP695k-2dJ-OutbGuuMxxUMlFH7038Dymmkw&usqp=CAU'
+                }
+                fluid
+              />
+            </Col>
+            <Col>
+              <Card>
+                <Card.Header as="h5">{movie.title}</Card.Header>
+                <Card.Body>
+                  <Card.Text> {movie.plot} </Card.Text> ​​
+                  {props.user && (
+                    <Link to={'/movies/' + props.match.params.id + '/review'}>
+                      Add Review
+                    </Link>
+                  )}
+                </Card.Body>
+              </Card>
+              <br></br> <h2>Reviews</h2>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </>
   );
 };
